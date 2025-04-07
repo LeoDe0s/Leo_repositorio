6826ad401857db6ae22e949976b702a08f689fe7
LeoDe0s <orue.leo444@gmail.com>
2025-04-07 16:35:45 -0300

    cods nuevos

---
+++ estructura de datos/cafetera/Cafetera/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Cafetera
+{
+    internal class Program
+    {
+        public class cafetera 
+        { 
+            public int _cpm { set; get; }
+            public int _ca { set; get; }
+
+            public cafetera() 
+            {
+                this._cpm = 1000;
+                this._ca = 0;
+            }
+
+            public cafetera (int capmax)
+            {
+                this._cpm = capmax;
+                this._ca = capmax;
+            }
+
+            public cafetera(int capmax, int cantac)
+            {
+                this._cpm = capmax;
+                if (cantac > capmax)
+                {
+                    cantac = capmax;
+                }
+                this._ca = cantac;
+            }
+
+            public void llenar_cafetera()
+            {
+                this._ca = this._cpm;
+            }
+
+            public void vaciar_cafetera()
+            {
+                this._ca = 0;
+            }
+
+            public void servir_taza(int cant)
+            {
+                if (cant < this._ca)
+                {
+                    this._ca = this._ca - cant;
+                }
+                else
+                {
+                    this._ca = 0;
+                }
+            }
+
+            public void agregar_cafe(int cant)
+            {
+                this._ca = this._ca + cant;
+                if(this._ca > this._cpm)
+                {
+                    this._ca = this._cpm;
+                }
+            }
+
+            public string Imprimir()
+            {
+                string txt;
+                txt = "la cantidad de la cafetera es:" + this._ca;
+                return txt;
+            }
+
+        }
+
+            static void Main(string[] args)
+        {
+            cafetera c1, c2, c3;
+            c1 = new cafetera(900,50);
+            c2 = new cafetera(900,200);
+            c3 = new cafetera(900,600);
+
+
+
+            Console.WriteLine(c1.Imprimir());
+            Console.WriteLine(c2.Imprimir());
+            Console.WriteLine(c3.Imprimir());
+        }
+    }
+}
---
+++ estructura de datos/numeros complejos/Numeros complejos/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+//crear una clase que permita trabajar con numeros complejos (parte real e imaginarias) incluyendo los siguientes metodos:
+//constructor, suma, resta, multiplicacion y division y un metodo para imprimir el numero por pantalla.
+namespace Numeros_complejos
+{
+    internal class Program
+    {
+
+        class NC 
+        {
+            public double Nreal;
+            public double Nimag;
+
+            public NC(double Nreal, double Nimag)
+            {
+                this.Nreal = Nreal;
+                this.Nimag = Nimag;
+            }
+
+            public void sumar(double x, double y)
+            {
+                Nreal = Nreal + x;
+                Nimag = Nimag + y;
+            }
+
+            public void resta(double x, double y)
+            {
+                Nreal = Nreal - x;
+                Nimag = Nimag - y;
+            }
+
+            public void multiplicacion(double x, double y)
+            {
+                Nreal = Nreal * x;
+                Nimag = Nimag * y;
+            }
+
+            public void division(double x, double y)
+            {
+                Nreal = Nreal / x;
+                Nimag = Nimag / y;
+            }
+
+            public string imprimir()
+            {
+                return $"Número complejo: {Nreal} y {Nimag}";
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            NC A = new NC (6,7);
+            NC B = new NC (10,5);
+
+           
+            B.sumar (A.Nreal, A.Nimag);
+            Console.WriteLine (B.imprimir());
+            
+        }
+    }
+}
